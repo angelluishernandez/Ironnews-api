12a0982e1f60cddda5f327d3680410490ce16f19
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-02-10 20:22:54 +0100

    seeds need upgrading

--- config/routes.js
+++ config/routes.js
@@ -10,6 +10,9 @@ const upload = require("./cloudinary.config")
 
 router.get('/', controller.base);
 router.get("/users", userController.getUsers)
+router.get("/folders", newsController.listFolders)
+
+
 router.post("/register", upload.single("profilePic"), authMiddleware.isNotAuthenticated, userController.create)
 router.post("/login", authMiddleware.isNotAuthenticated, userController.doLogin)
 router.post("/logout", authMiddleware.isAuthenticated, userController.logout)
--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,12 +1,20 @@
 const News = require("../models/news.model");
 const Users = require("../models/user.model");
+const Folders = require("../models/folder.model");
 const createError = require("http-errors");
 
 module.exports.listNews = (req, res, next) => {
-	console.log(req.params.id)
+	console.log(req.params.id);
 	Users.findById(req.params.id)
 		.populate("folders")
 		.populate("news")
 		.then(user => res.json(user.folders))
+		.catch(error => console.info(error));
+};
+
+module.exports.listFolders = (req, res, next) => {
+	Folders.find()
+		.populate("news")
+		.then(folder => res.json(folder))
 		.catch(error => console.info(error));
 };
