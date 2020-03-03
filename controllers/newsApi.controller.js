c648533db56316f508b9ebcbe019c58774c4bb0c
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-03-03 10:22:41 +0100

    latest news working with dates

--- controllers/newsApi.controller.js
+++ controllers/newsApi.controller.js
@@ -18,15 +18,30 @@ module.exports.landing = (req, res, next) => {
 
 module.exports.getTopHeadlines = (req, res, next) => {
 	const { query, qInTitle, language, source, from, to, sortBy } = req.body;
-	console.log("this is the req.body=>", req.body);
+
+
+	const formatDates = (date)=>{
+		return date.getUTCFullYear() + "-" +
+    ("0" + (date.getUTCMonth()+1)).slice(-2) + "-" +
+    ("0" + date.getUTCDate()).slice(-2) 
+	}
+	
+	const currentDate =  new Date(Date.now())
+	const yesterday = new Date(currentDate.setDate(currentDate.getDate() - 1))
+
+	const formatedCurrentDate = formatDates(currentDate)
+	const formatedYesterday = formatDates(yesterday)  
+	console.log("this is the current date=> ", formatedCurrentDate)
+	console.log("this is yesterday=>", formatedYesterday)
+
 	newsapi.v2
 		.everything({
 			q: query,
 			qInTitle: qInTitle,
 			language: language ,
 			sources: source,
-			from: from ? from : "",
-			to: to ? from : "",
+			from: formatedCurrentDate,
+			to: formatedYesterday,
 			sortBy: sortBy ,
 			pageSize: 100,
 		})
