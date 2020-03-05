a920760ca1ef7e32f514e5507f2fa61942178dee
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-03-05 14:47:56 +0100

    .

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,7 @@
 const cors = require('cors')
 
 const corsMiddleware = cors({
-	origin: ["https://ironnewsapp.herokuapp.com/"],
+	origin: process.env.REACT_APP_API_URL,
 	allowedHeaders: ['Content-Type', 'Origin'], 
 	credentials: true
 })
