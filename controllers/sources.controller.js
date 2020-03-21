d54b63f47c0dbf6cf9ecb8a421693c3b9d594581
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-03-21 13:42:45 +0100

    getSources works

--- controllers/sources.controller.js
+++ controllers/sources.controller.js
@@ -4,7 +4,6 @@ const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 // List Sources
 
 module.exports.getSources = (req, res, next) => {
-	console.log(req.body);
 	const { category, language, country } = req.body;
 
 	newsapi.v2
@@ -14,7 +13,6 @@ module.exports.getSources = (req, res, next) => {
 			country: country,
 		})
 		.then(response => {
-			console.log(response.sources);
 			res.status(200).json(response.sources);
 		})
 		.catch(error => console.log(error));
